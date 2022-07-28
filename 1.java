e33ad9907a13005efda97684166f3d1eb08b824a
Preetika <preetjagota01@gmail.com>
2022-07-28 14:08:44 +0530

    advance code

--- 1.java
+++ 1.java
@@ -1,1 +1,4 @@
 this is my java file  code written
+There are ssome chaanges in this Im adding them write now please  see towards them add per your requirements
+these are advance changes in tge code
+numpy as np
